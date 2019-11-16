761c65f105fc2f99edaeb546fd539e5596bfcc3d
anandteerth1999 <anandmathad446@gmail.com>
2019-11-16 08:17:50 +0530

    Added Hod Component Details

--- src/app/hod/hod.component.ts
+++ src/app/hod/hod.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Faculty } from '../fcd';
+import { MatPaginator, MatTableDataSource } from '@angular/material';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-hod',
@@ -6,10 +10,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hod.component.css']
 })
 export class HodComponent implements OnInit {
-
-  constructor() { }
+  faculty:any;
+  dataSource : any;
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  displayedColumns: string[] = ['name'];
+  
+  constructor( private route: ActivatedRoute,private http:HttpClient, private router: Router) { }
 
   ngOnInit() {
-  }
+    this.http.get<Faculty[]>("http://localhost:5000/HOD_Leave").subscribe(data =>{
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator;
+  });
+}
 
 }
