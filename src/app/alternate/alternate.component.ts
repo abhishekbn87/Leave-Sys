b21594f080455822f0e21f9a3df5e46fc4f6820c
anandteerth1999 <anandmathad446@gmail.com>
2019-11-19 15:18:57 +0530

    Implemented Alternate Component

--- src/app/alternate/alternate.component.ts
+++ src/app/alternate/alternate.component.ts
@@ -3,6 +3,7 @@ import { MatPaginator, MatTableDataSource } from '@angular/material';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { Handle } from './handle';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 
 @Component({
@@ -12,28 +13,55 @@ import { Handle } from './handle';
 })
 export class AlternateComponent implements OnInit {
   alter: Number;
-  hands:Handle[];
-  date1:any;
-  class:any;
-  section:any;
-  time:any;
-  sub:any;
-  handel:any;
-  constructor (private http:HttpClient) {
+  hands:Handle[]=[];
+  date1:String[]=[];
+  class1:String[]=[];
+  section:String[]=[];
+  time:String[]=[];
+  sub:String[]=[];
+  handel:Number[]=[];
+  alt:Number[] = [];
+  email:String;
+  constructor (private http:HttpClient, af:AngularFireAuth,private router:Router) {
+    af.authState.subscribe(user=>{
+      this.email = user.email;
+    })
+
 
   }
 
   ngOnInit() {
-    this.http.get<Handle[]>("http://localhost;5000/handle").subscribe(data=>{
+    this.http.get<Handle[]>("http://localhost:5000/handle").subscribe(data=>{
       this.hands = data;
     });
-    /*this.http.get<Number>("http://localhost:5000/alternate").subscribe(data =>{
+    this.http.get<Number>("http://localhost:5000/alternate/"+this.email).subscribe(data =>{
       this.alter = data;
       for(let i =1;i<=data;i++){
         this.alt.push(i);
       }
-    });*/
+    });
 
+  }
+
+  trackByIndex(index: number, obj: any): any {
+    return index;
+  }
+
+  success(){
+    console.log(this.email);
+    console.log(this.date1);
+    console.log(this.class1);
+    console.log(this.section);
+    console.log(this.time);
+    console.log(this.sub);
+    console.log(this.handel);
+    console.log(this.alter);
+
+    for(let i=0;i<this.alter;i++){
+      console.log(i);
+      this.http.get("http://localhost:5000/altinsert/"+this.email+"/"+this.date1[i]+"/"+this.class1[i]+"/"+this.section[i]+"/"+this.time[i]+"/"+this.sub[i]+"/"+this.handel[i],{observe:'response'}).subscribe(response=>console.log(response))
+    }
+  this.router.navigateByUrl("/home")
   }
 
 }
--- src/app/leave-details/leave-details.component.ts
+++ src/app/leave-details/leave-details.component.ts
@@ -7,25 +7,6 @@ import { HttpClient } from '@angular/common/http';
 import { LeaveD } from 'src/app/leave'
 import { MatPaginator, MatTableDataSource } from '@angular/material';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
 
 /**
  * @title Basic use of `<table mat-table>`
@@ -37,13 +18,16 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class LeaveDetailsComponent {
   leave: LeaveD[];
+  email:String;
   dataSource : any;
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   displayedColumns: string[] = ['Slno','Lid','nod_applied','nod_remaining'];
-  constructor( private route: ActivatedRoute,private http:HttpClient, private router: Router) { }
+  constructor( private route: ActivatedRoute,private http:HttpClient, private router: Router,af:AngularFireAuth) {
+    af.authState.subscribe(user=>this.email = user.email)
+   }
 
   ngOnInit() {
-    this.http.get<LeaveD[]>("http://localhost:5000/leaved").subscribe(data =>{
+    this.http.get<LeaveD[]>("http://localhost:5000/leaved/"+this.email).subscribe(data =>{
       this.dataSource = new MatTableDataSource(data);
       this.dataSource.paginator = this.paginator;
     });
